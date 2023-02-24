1c509f075286653c4abd439810479582d901a192
MarsuPila <22983240+MarsuPila@users.noreply.github.com>
2023-02-24 10:53:58 +0100

    normalisation

--- dataloader_test.py
+++ dataloader_test.py
@@ -1,4 +1,5 @@
 import sys
+import os.path
 import logging
 from utils import logging_utils
 logging_utils.config_logger()
@@ -16,10 +17,11 @@ class Params():
     two_step_training = True
     orography = False
     add_noise = False
-    normalize = False
-    global_means_path = '/scratch/f1000/dealmeih/ds/FCN/FCN_ERA5_data_v0/global_means.npy'
-    global_stds_path  = '/scratch/f1000/dealmeih/ds/FCN/FCN_ERA5_data_v0/global_stds.npy'
+    normalize = True
+    global_means_path = None #'/scratch/f1000/dealmeih/ds/FCN/FCN_ERA5_data_v0/global_means.npy'
+    global_stds_path  = None #'/scratch/f1000/dealmeih/ds/FCN/FCN_ERA5_data_v0/global_stds.npy'
     add_grid = False
+    normalization = 'zscore'
 
     # invars_2d = ['U_10M', 'V_10M', 'PMSL', 'PS', 'T_2M']
     # invars_3d = ['FI', 'T', 'U', 'u_100', 'V', 'v_100', 'RELHUM']
@@ -28,6 +30,9 @@ if __name__ == "__main__":
     params = Params()
     if sys.argv[1] == 'cosmo':
         params.era5 = False
+        if params.normalize:
+            params.global_means_path = os.path.join(str(sys.argv[2]), 'global_means.npy')
+            params.global_stds_path  = os.path.join(str(sys.argv[2]), 'global_stds.npy')
         dataset = GetCosmoDataset(params, str(sys.argv[2]), train=False)
     else: # era5
         params.era5 = True
